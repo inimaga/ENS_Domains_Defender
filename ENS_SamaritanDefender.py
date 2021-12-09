eb3a3cc54fe44e7609cceaba60d28d9e2636390a
Issa Nimaga <issa.nimaga@gmail.com>
2021-12-09 09:07:08 +0000

    Added a text file to the repo from which words would be selected at random to form the seed phrase. Seed phrases now posted to phishing sites are now much more varied.

--- ENS_SamaritanDefender.py
+++ ENS_SamaritanDefender.py
@@ -3,9 +3,11 @@
 
 ### Identified phishing sites:
 ###     - https://ensdomain.online
+###     - https://claimens.domains/
 
 import requests
 import threading
+import random
 
 ##### Endpoint of the phishing sites, where the results are posted to once credentials have been collected
 #url = "https://ensdomain.online/metamask/action_page.php" # Ensdomain.online is no longer online and has been taken down
@@ -25,58 +27,58 @@ wallets = [
 
 # 2 - List of userAgents (i.e: Browser/Platform)
 # Todo: Add multiple varied userAgents, so entries posted to phishing database simulate being collected from a variety of platforms (not just Macs)
+# Good source is: https://developers.whatismybrowser.com/useragents/explore/ althgough not very scalable. Maybe chuck a bunch of them into a file like the word bank below?
 userAgents = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36"
+    "Mozilla/5.0 (Macintosh; Intel Mac OS) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
+    "Mozilla/5.0 (X11; CrOS x86_64 13597.94.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.186 Safari/537.36"
     ]
 
-# 3 - List of seed phrases to post to the database (i.e: Fake passwords)
-# Todo: Write a function that generates random words to use as seed phrases so results posted are more varied
-seeds = [
-    'Oii dickheads stop scamming people. why meat orange are mango expecting to with this mango Twat wankers bellend',
-    'immense wanker helpful severe popular you superb skinny general twat cute important',
-    'conscious weak lumpy craven hard terrible you scammers sedate beneficial truthful accidental',
-    'puffy wanker wry dependent bawdy oi healthy painful scarce stop angry damp abhorrent scam quizzical craven bored lean',
-    'bellend sending bunch of seemingly legit request you will never know which is right quizzical craven yasoy lean',
-    'Gonna Run wry this bawdy every healthy day to screw yoy damp abhorrent quickest quizzical craven wenin tlir',
-    'random generated online words smoggy online hesitant internal bad financial sweet rich woebegone well-made nippy harsh minor amuck utter draconian slimy lewd sexual witty',
-    'long steady futuristic popular spiteful clean fascinated earthy decisive sassy savory grumpy adjoining flimsy mixed frail burly naughty imminent puffy keen like reasonable tiresome'
-    ]
+# 3 - Seed phase prep from which to post to the database (i.e: Fake passwords)
+with open("RandomWordBank.txt", "r") as file:
+    text = file.read()
+    words = list(map(str, text.split())) #This is the bank from which the words will be selected at random to make the seed phrase
+seed_phrase_length = 18 #Could be either 12, 18 or 24
 
 # Method/Function for sending/posting the requests
 def send_requests():
     while True: # Run forever until interrupted
-        for seed in seeds: 
-            for wallet in wallets:
-                for userAgent in userAgents:
-                    # Looping though the list of seeds, userAgents and Wallets and creating a different combination of data to be sent
-                    data = {
-                        #"seed": seed,
-                        #"wallet": wallet
-                        "pk": seed,
-                        "walletselected": wallet,
-                        "btn1":"",
-                        }
-                    headers = {
-                        "Host": "claimens.domains",
-                        "User-Agent": userAgent,
-                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                        "Accept-Language": "en-GB,en;q=0.5",
-                        "Accept-Encoding": "gzip, deflate, br",
-                        "Referer": "https://claimens.domains/Ens/wallet-RD266-loadingAdd-frontchange/",
-                        "Content-Type": "application/json",
-                        "Origin": "https://claimens.domains",
-                        "Content-Length": "44",
-                        "DNT": "1",
-                        "Connection": "keep-alive",
-                        "Upgrade-Insecure-Requests": "1",
-                        "Sec-Fetch-Dest": "document",
-                        "Sec-Fetch-Mode": "navigate",
-                        "Sec-Fetch-Site": "same-origin",
-                        "Sec-Fetch-User": "?1"
-                        }
-                    response = requests.post(url = url, data = data, headers = headers)
-                    print(wallet, response) # Upon completion of a request, print to terminal giving visual feedback
+        for wallet in wallets:
+            for userAgent in userAgents:
+                # Looping though the list of userAgents and wallets and creating a different combination of data to be sent
+                array_of_random_seeds = random.sample(words, seed_phrase_length)
+                seed_phrase = ' '.join(array_of_random_seeds)
+                data = {
+                    #"seed": seed_phrase,
+                    #"wallet": wallet
+                    "pk": seed_phrase,
+                    "walletselected": wallet,
+                    "btn1":"",
+                    }
+                headers = {
+                    "Host": "claimens.domains",
+                    "User-Agent": userAgent,
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                    "Accept-Language": "en-GB,en;q=0.5",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Referer": "https://claimens.domains/Ens/wallet-RD266-loadingAdd-frontchange/",
+                    "Content-Type": "application/json",
+                    "Origin": "https://claimens.domains",
+                    "Content-Length": "44",
+                    "DNT": "1",
+                    "Connection": "keep-alive",
+                    "Upgrade-Insecure-Requests": "1",
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    "Sec-Fetch-Site": "same-origin",
+                    "Sec-Fetch-User": "?1"
+                    }
+                response = requests.post(url = url, data = data, headers = headers)
+                print(wallet, response) # Upon completion of a request, print to terminal giving visual feedback
 
 ## Multi-threading the script
 threads = []
@@ -92,6 +94,5 @@ for i in range(num_of_threads):
 
 for i in range(num_of_threads):
     threads[i].join()
-
 
 ### Final note: This script is over-commented on purpose so others can easily pick it up and run wilder with it
